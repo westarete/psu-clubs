72521fbf61b8c2dffaeec7eb90783e707c71eeda
Scott Woods <scott@westarete.com>
2015-03-28 16:40:59 -0400

    Visit each organization's page

--- crawl.rb
+++ crawl.rb
@@ -13,9 +13,24 @@ Capybara.register_driver :poltergeist do |app|
 end
 Capybara.current_driver = :poltergeist
 
-visit "/hub/studentorgs/orgdirectory/"
-select "2", from: "Skip to Page:"
-click_button "Go"
+1.upto(71) do |page_number|
 
-save_screenshot 'page2.png'
+  # Do this for all 15 links on the page
+  15.times do |link_index|
 
+    visit "/hub/studentorgs/orgdirectory/"
+    select page_number.to_s, from: "Skip to Page:"
+    click_button "Go"
+
+    # Go get the Nth link on the page
+    link = page.all('table#ctl00_ContentPlaceHolder1_GridViewResults tr a')[link_index]
+
+    next if ["Org Name", "President Name", "Category"].include?(link.text)
+
+    link.click
+
+    p find('#ctl00_ContentPlaceHolder1_LabelOrgName').text
+
+  end
+
+end
